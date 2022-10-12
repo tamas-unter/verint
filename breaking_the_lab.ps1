5ae1c80d7e4d0cbe6bc56eb299ca00daa6dc3b31
Pásztor Tamás <36480720+tamas-unter@users.noreply.github.com>
2022-10-12 11:58:21 -0400

    Add files via upload
    not working

--- breaking_the_lab.ps1
+++ breaking_the_lab.ps1
@@ -1,4 +1,4 @@
-﻿<# v0.9 10/11/2022
+﻿<# v1.1 10/12/2022
 Scripts to break (and fix) recorder functionality - for training purposes
 
 
@@ -54,7 +54,7 @@ Function Check-Elevation{
 
 
 Function Get-ServerAddressByRole{
-param([Parameter(Mandatory=$true)] $role)
+	param([Parameter(Mandatory=$true)] $role)
     [xml]$s=cat "$env:IMPACT360SOFTWAREDIR\Conf\Cache\Servers.xml"
     $ns=@{svr=$s.Servers.svr}
     $s|select-xml -Namespace $ns "//*/svr:ServerRole[@Name='$role']/../@Hostname"|
@@ -101,161 +101,173 @@ Function Restore-ConfFile{
 
 Function Conf-2-Temp{param($confFile) $env:TEMP +"\"+ $confFile.Replace("\","_").replace("$","-colon-").replace(":","-colon-")}
 
-############# EXCERCISE 1 #################### 
-Function Break-AvayaMRPassCode{
-    $role="IP_RECORDER"
-	$confFile="\e$\Impact360\Software\conf\IntegrationService.xml"
-	$services="Recorder Integration Service"
-	
-	Get-ServerAddressByRole $role|%{
-		Stop-RecorderService -server $_ -service $services
-		$cffull="\\$_$confFile"
-		Backup-confFile $cffull
-		[xml]$is=cat $cffull
-		(($is.IFService.Integrations.Integration|where type -eq "AvayaCMAPIAdapter").set|
-			where key -eq DevicePassCode).value="0123457777"	
-		$is.Save($cffull)
-		Update-Signature $cffull
-		Start-RecorderService -server $_ -service $services
-	}
+Function Do-BreakExcercise{
+	param($sender,$e)
+	$index=$sender.TabIndex
+	Write-Host "Breaking excercise {0}" -f $index
+#	Break-Config -role $Global:excercises[$index].role -confFile $Global:excercises[$index].confFile -services $Global:excercises[$index].services -breakFunction $Global:excercises[$index].breakFunction
 }
-
-Function Fix-AvayaMRPassCode{
-    $role="IP_RECORDER"
-	$confFile="\e$\Impact360\Software\conf\IntegrationService.xml"
-	$services="Recorder Integration Service"
-	Get-ServerAddressByRole $role|%{
-		Stop-RecorderService -server $_ -service $services
-		Restore-ConfFile "\\$_$confFile"
-		Start-RecorderService -server $_ -service $services
-	}
-}
-
-############# EXCERCISE 2 #################### 
-#TODO 	2. NIC configuration -- multiple ones GUID? Wrong guid  <x:Adapters><x:Adapter><x:AdapterId>\Device\NPF_{6D290CBC-C8B7-4FF9-99E7-7C48FFA6C961}</x:AdapterId><x:AdapterName>VMware vmxnet3 virtual network device</x:AdapterName>
-
-Function Break-NICConfiguration{
-	$role="IP_RECORDER"
-	$confFile="\e$\Impact360\Software\contactstore\IPCaptureConfig.xml"
-	$services="Recorder IP CaptureEngine"
-	Get-ServerAddressByRole $role|%{
-		Stop-RecorderService -server $_ -service $services
-		$cffull="\\$_$confFile"
-		Backup-confFile $cffull
-		[xml]$ipc=cat $cffull
-		($ipc.captureconfiguration.Adapters.Adapter|where Mode -eq "Delivery").AdapterId = "\Device\NPF_{6D290CBC-C8B7-4FF9-99E7-0000000000}"
-		$ipc.Save($cffull)	
-		
-		Update-Signature $cffull
-		Start-RecorderService -server $_ -service $services
-	}
-}
-Function Fix-NICConfiguration{
-    $tempFile="$env:TEMP\ipc.xml"
-    mv -Force $tempFile "$(env:impact360softwaredir)\contactstore\IPCaptureConfig.xml"
-	Restart-RecorderService $role "Recorder IP CaptureEngine"
-}
-############# EXCERCISE 3 #################### 
-# 3c media definition - break the NAS string
-Function Break-ArchiveMedia{
-param ($role="ENTERPRISE_ARCHIVER")
-    $confFile="$env:IMPACT360SOFTWAREDIR\ContactStore\ArchiverConfig.xml"
-    Stop-RecorderService $role "archiver"
-    $tempFile="$env:TEMP\arc.xml"
-    mv $confFile $tempFile
-    [xml]$a=cat $tempFile
-    $a.archiver.Devices.Device.DeviceTargets.DeviceTarget.PhysicalDevice="G:\archifoobar"
-    $a.Save($confFile)
-    Update-Signature $confFile
-    Start-RecorderService $role "archiver"
-}
-Function Fix-ArchiveMedia{
-param ($role="ENTERPRISE_ARCHIVER")
-    $tempFile="$env:TEMP\arc.xml"
-	Stop-RecorderService $role "archiver"
-    mv -Force $tempFile "$env:IMPACT360SOFTWAREDIR\ContactStore\ArchiverConfig.xml"
-	Start-RecorderService $role "archiver"
+Function Do-FixExcercise{
+	param($sender,$e)
+	$index=$sender.TabIndex
+	Write-Host "Fixing excercise {0}" -f $index
+#	Fix-Config -role $Global:excercises[$index].role -confFile $Global:excercises[$index].confFile -services $Global:excercises[$index].services 
 }
 
 
-############# EXCERCISE 4 #################### 
-#TODO	4. delete the buffer config for disk manager so that calls are not stored anymore
-
-Function Break-CallsBuffer{
-param($role="IP_RECORDER")
-    $tempFile="$env:TEMP\rg.xml"
-    $confFile="$env:IMPACT360SOFTWAREDIR\ContactStore\RecorderGeneral.xml"
-    ###stop ip capture, screen capture
-	Stop-RecorderService $role "capture"
-    mv $confFile $tempFile
-    [xml]$r=cat $tempFile
-	
-	Start-RecorderService $role "capture"
-}
-Function Fix-CallsBuffer{
-param($role="IP_RECORDER")
-    $tempFile="$env:TEMP\rg.xml"
-    Stop-RecorderService $role "capture"
-	mv -Force $tempFile "$env:IMPACT360SOFTWAREDIR\ContactStore\RecorderGeneral.xml"
-	Start-RecorderService $role "capture"
-}
-############# EXCERCISE 5 #################### 
-#5. TSLIB.ini file modification to break connection with aes
-
-Function Break-TSAPI{
-param($role="INTEGRATION_FRAMEWORK")
-	$tempFile="$env:TEMP\tslib.ini"
-	$confFile= 'C:\Program Files (x86)\Avaya\AE Services\TSAPI Client\TSLIB.INI'
-	Stop-RecorderService $role "recorder integration"
-	$content=[System.IO.File]::ReadAllText($confFile)
-	$c=$content.split("`n") #|where {$_ -notmatch "^;"}|where {$_.length -gt 0}
-	for ($i=0;$i -lt $c.Count;$i++){
-		if($c[$i] -match "^[^;].*=450") {$c[$i]="0.1.2.3=450"}
-	}
-	[System.IO.File]::WriteAllText(
-		$confFile, (
-			($c |where {[string]::IsNullOrEmpty($_) -eq $false}) -join "`r`n"
-		)
+Function Fix-Config{
+	param(
+		$role,
+		$confFile,
+		$services,
+		$fixFunction
 	)
-	Start-RecorderService $role "recorder integration"
-	
-}
-Function Fix-TSAPI{
-param($role="INTEGRATION_FRAMEWORK")
-	$tempFile="$env:TEMP\tslib.ini"
-	## TODO!! network location - \\hostname\c$\ ...
-	$confFile= 'C:\Program Files (x86)\Avaya\AE Services\TSAPI Client\TSLIB.INI'
-	Stop-RecorderService $role "recorder integration"
-	mv -Force $tempFile $confFile
-	Start-RecorderService $role "recorder integration"
-}
-############# EXCERCISE 6 #################### 
-#TODO 6. remove extensions from data source. 
-
-Function Break-Extensions{
-}
-Function Fix-Extensions{
-}
-############# EXCERCISE 7 #################### 
-#TODO SCM registry update -- already done, check INNO LAB
-### this one needs to run on a remote PC via Remote Registry service. if that is not enabled, we'll need to Remote PS
-function Break-SCMRis{
-param($hostName="ADVDSK9")
-    Invoke-Command -ComputerName $hostName {
-        get-service *screencapture*|stop-service
-        $oldRIS=Get-ItemProperty -path 'HKLM:\SOFTWARE\WOW6432Node\Witness Systems\eQuality Agent\Capture\CurrentVersion\' -name IntegrationServicesServersList
-
-        
-        $newRIS="dummy.wfo.verint.training:29522"
-        Set-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\Witness Systems\eQuality Agent\Capture\CurrentVersion\' -name IntegrationServicesServersList -Value $newRIS
-        Start-Service *scree
-    }
-}
-function Fix-SCMRis{
-
+	if ($fixFunction -eq $null) {
+		Get-ServerAddressByRole $role|%{
+			Stop-RecorderService -server $_ -service $services
+			Restore-ConfFile "\\$_$confFile"
+			Start-RecorderService -server $_ -service $services
+		}
+	}	else {
+		Invoke-Command -ArgumentList $confFile -ScriptBlock $fixFunction
+	}
 }
 
+Function Break-Config{
+	param(
+		$role,
+		$confFile,
+		$services,
+		$breakFunction
+	)	
+	if($role -ne $null) {
+		Get-ServerAddressByRole $role|%{
+			Stop-RecorderService -server $_ -service $services
+			$cffull="\\$_$confFile"
+			Backup-confFile $cffull
+			
+			Invoke-Command -ArgumentList $cffull -ScriptBlock $breakFunction
+			
+			Update-Signature $cffull
+			Start-RecorderService -server $_ -service $services
+		}
+	} else {
+		Invoke-Command -ArgumentList $confFile -ScriptBlock $breakFunction
+		
+	}
+}
 
+# in this array, each element is containing the definition of the excercise. In case the $role is missing ($null), $confFile will be the argument for the breakFunction
+## BEGINNING of excercise definition
+$Global:excercises=@(
+	#0: AvayaMRPassCode
+	@{
+		role="IP_RECORDER"
+		confFile="\e$\Impact360\Software\conf\IntegrationService.xml"
+		services="Recorder Integration Service"
+		breakFunction={
+			param($cffull)
+			[xml]$is=cat $cffull
+			(($is.IFService.Integrations.Integration|where type -eq "AvayaCMAPIAdapter").set|
+				where key -eq DevicePassCode).value="0123457777"	
+			$is.Save($cffull)
+		}
+	},
+	#1: NICConfiguration
+	@{
+		role="IP_RECORDER"
+		confFile="\e$\Impact360\Software\contactstore\IPCaptureConfig.xml"
+		services="Recorder IP CaptureEngine"
+		breakFunction={
+			param($cffull)
+			[xml]$ipc=cat $cffull
+			($ipc.captureconfiguration.Adapters.Adapter|where Mode -eq "Delivery").AdapterId = "\Device\NPF_{6D290CBC-C8B7-4FF9-99E7-0000000000}"
+			$ipc.Save($cffull)
+		}
+	},
+	#2: ArchiveMedia
+	@{
+		role="ENTERPRISE_ARCHIVER"
+		confFile="\e$\Impact360\Software\ContactStore\ArchiverConfig.xml"
+		services="archiver"
+		breakFunction={
+			param($cffull)
+			[xml]$a=cat $cffull
+			$a.archiver.Devices.Device.DeviceTargets.DeviceTarget.PhysicalDevice="G:\archifoobar"
+			$a.Save($cffull)
+		}
+	},
+	#3: CallsBuffer
+	@{
+		role="IP_RECORDER"
+		confFile="\e$\Impact360\Software\ContactStore\RecorderGeneral.xml"
+		services= "capture"
+		breakFunction={
+			param($cffull)
+			[xml]$r=cat $cffull
+			$r.recordergeneral.callspath="X:\NoCallsHere"
+			$r.Save($cffull)
+		}
+	},
+	#4: TSAPI
+	@{
+		role="INTEGRATION_FRAMEWORK"
+		confFile='\C$\Program Files (x86)\Avaya\AE Services\TSAPI Client\TSLIB.INI'
+		services= "recorder integration"
+		breakFunction={
+			param($cffull)
+			$content=[System.IO.File]::ReadAllText($cffull)
+			$c=$content.split("`n") #|where {$_ -notmatch "^;"}|where {$_.length -gt 0}
+			for ($i=0;$i -lt $c.Count;$i++){
+				if($c[$i] -match "^[^;].*=450") {$c[$i]="0.1.2.3=450"}
+			}
+			[System.IO.File]::WriteAllText(
+				$cffull, (
+					($c |where {[string]::IsNullOrEmpty($_) -eq $false}) -join "`r`n"
+				)
+			)			
+		}
+	},
+	########TODO
+	#5: Extensions
+	@{
+		role="INTEGRATION_FRAMEWORK"
+		confFile=""
+		services="recorder integration"
+		breakFunction={
+			param($cffull)
+			
+		}
+	},
+	#6: SCMRis
+	@{
+		confFile='HKLM:\SOFTWARE\WOW6432Node\Witness Systems\eQuality Agent\Capture\CurrentVersion\'
+		breakFunction={
+			param($regPath)
+			Invoke-Command -ComputerName "ADVDSK9" {
+				get-service *screencapture*|stop-service
+				$oldRIS=Get-ItemProperty -path $regPath -name IntegrationServicesServersList
+				$oldRIS|out-file $env:TEMP\oldris.txt
+				
+				$newRIS="dummy.wfo.verint.training:29522"
+				Set-ItemProperty -Path $regPath -name IntegrationServicesServersList -Value $newRIS
+				Start-Service *scree
+			}
+			
+		}
+		fixFunction={
+			param($regPath)
+			Invoke-Command -ComputerName "ADVDSK9" {
+				get-service *screencapture*|stop-service
+				$newRIS=cat $env:TEMP\oldris.txt
+				Set-ItemProperty -Path $regPath -name IntegrationServicesServersList -Value $newRIS
+				Start-Service *scree
+			}
+		}
+	}
+)
+
+## END of excercise definition
 
 ############# EXCERCISE 8 #################### 
 #TODO 8. remove hunt group from DS to produce tagging issue
@@ -290,7 +302,7 @@ if ((Check-Elevation) -eq $false) {
 # $Global:STATES=@(@($true,$false),@($true,$false),@($true,$false),@($true,$false))
 
 ############### GUI
-$excercise_count=11
+$excercise_count=$Global:excercises.Count
 
 Add-Type -AssemblyName System.Windows.Forms
 
@@ -312,22 +324,17 @@ $fixers=New-Object System.Windows.Forms.Button[] $excercise_count
 
     $breakers[$_]=New-Object System.Windows.Forms.Button
     $breakers[$_].Text="Break"
+    $breakers[$_].TabIndex=$_
     $breakers[$_].Location=New-Object System.Drawing.Point(40,(15+($_*30)))
+	$breakers[$_].Add_Click({Do-BreakExcercise $this $_})
 
     $fixers[$_]=New-Object System.Windows.Forms.Button
     $fixers[$_].Text="Fix"
+	$fixers[$_].TabIndex=$_
     $fixers[$_].Location=New-Object System.Drawing.Point(120,(15+($_*30)))
+	$fixers[$_].Add_Click({Do-FixExcercise $this $_})
     
 }
-
-$breakers[0].Add_Click({Break-AvayaMRPassCode});
-$fixers[0].Add_Click({Fix-AvayaMRPassCode});
-$breakers[1].Add_Click({Break-NICConfiguration});
-$fixers[1].Add_Click({Fix-NICConfiguration});
-$breakers[2].Add_Click({Break-ArchiveMedia});
-$fixers[2].Add_Click({Fix-ArchiveMedia});
-$breakers[3].Add_Click({Break-CallsBuffer});
-$fixers[3].Add_Click({Fix-CallsBuffer});
 
 $f.Controls.AddRange($labels)
 $f.Controls.AddRange($breakers)
