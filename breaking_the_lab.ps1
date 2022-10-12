47094454a8c0dddbbb27c0dd4c3f01f407857253
Pásztor Tamás <36480720+tamas-unter@users.noreply.github.com>
2022-10-12 12:12:47 -0400

    fixed checksum, enabled functions

--- breaking_the_lab.ps1
+++ breaking_the_lab.ps1
@@ -62,10 +62,12 @@ Function Get-ServerAddressByRole{
 }
 
 Function Update-Signature{
-    param([Parameter(Mandatory=$true)][string] $configFile)
-	#this should be local...
-    if(test-path -PathType Leaf "$env:IMPACT360SOFTWAREDIR\ContactStore\ChecksumUtil.exe") {$cmd="$env:IMPACT360SOFTWAREDIR\ContactStore\ChecksumUtil.exe"} else
-		{$cmd="$env:IMPACT360SOFTWAREDIR\ContactStore\Tools\ChecksumUtil.exe"}
+    param(
+		[Parameter(Mandatory=$true)][string] $server,
+		[Parameter(Mandatory=$true)][string] $configFile
+	)
+    if(test-path -PathType Leaf "\\$server\e$\IMPACT360\SOFTWARE\ContactStore\ChecksumUtil.exe") {$cmd="\\$server\e$\IMPACT360\SOFTWARE\ContactStore\ChecksumUtil.exe"} else
+		{$cmd="\\$server\e$\IMPACT360\SOFTWARE\ContactStore\Tools\ChecksumUtil.exe"}
 	
     iex "$cmd -g $configFile"
     
@@ -105,13 +107,13 @@ Function Do-BreakExcercise{
 	param($sender,$e)
 	$index=$sender.TabIndex
 	Write-Host ("Breaking excercise {0}" -f $index)
-#	Break-Config -role $Global:excercises[$index].role -confFile $Global:excercises[$index].confFile -services $Global:excercises[$index].services -breakFunction $Global:excercises[$index].breakFunction
+	Break-Config -role $Global:excercises[$index].role -confFile $Global:excercises[$index].confFile -services $Global:excercises[$index].services -breakFunction $Global:excercises[$index].breakFunction
 }
 Function Do-FixExcercise{
 	param($sender,$e)
 	$index=$sender.TabIndex
 	Write-Host ("Fixing excercise {0}" -f $index)
-#	Fix-Config -role $Global:excercises[$index].role -confFile $Global:excercises[$index].confFile -services $Global:excercises[$index].services 
+	Fix-Config -role $Global:excercises[$index].role -confFile $Global:excercises[$index].confFile -services $Global:excercises[$index].services 
 }
 
 
@@ -148,7 +150,7 @@ Function Break-Config{
 			
 			Invoke-Command -ArgumentList $cffull -ScriptBlock $breakFunction
 			
-			Update-Signature $cffull
+			Update-Signature $_ $cffull
 			Start-RecorderService -server $_ -service $services
 		}
 	} else {
